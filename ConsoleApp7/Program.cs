1e1af1c6ab36cc318a9ea7da6d03b6a9f4730fa3
DzhumagazievaDiana <113889448+DzhumagazievaDiana@users.noreply.github.com>
2022-10-02 23:30:18 +0300

    Add files via upload

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int n = 0;
+        Console.WriteLine("Введите n:");
+        n = int.Parse(Console.ReadLine());
+        var p = n % 10;
+        var x = (n - p) / 10 + (p * 100);
+        Console.WriteLine("Трехзначное число равно ={0}", x);
+        Console.ReadKey();
+
+    }
+}
